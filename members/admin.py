7a9895938a42334e349e4e7fa1a03f7f514f813b
ANOLD DAVID <anold@ANOLD>
2024-09-07 21:08:35 +0300

    Add project files

---
+++ members/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Members
+# Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("firstname","lastname","date_joined")
+
+admin.site.register(Members, MemberAdmin)
